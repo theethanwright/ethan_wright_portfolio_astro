f89984cfc32fe77e0c2fe6875d94f29f0fea8845
theethanwright <theethanwright@gmail.com>
2024-05-29 10:05:51 -0400

    I've done a lot

---
+++ .history/public/work/public/app_20240528213153.js
@@ -0,0 +1,28 @@
+var dots = document.querySelectorAll(".preview_img_link");
+
+for(var i = 0; i < dots.length; i ++){
+dots[i].dotIndex = i;
+dots[i].addEventListener("click", function(){
+  console.log(this.dotIndex)
+  var a = this.dotIndex;
+  console.log(this.dotIndex);
+  const copied = document.getElementById(a)
+  const nodeHeight = copied.offsetHeight;
+  const nodeWidth = copied.offsetWidth;
+  const nodeTop = copied.getBoundingClientRect().top;
+  const nodeLeft = copied.getBoundingClientRect().left;
+  var clonedNode = copied.cloneNode(true);
+  clonedNode.id = "node_clone";
+  document.body.appendChild(clonedNode);
+  previewImg = clonedNode.querySelectorAll("preview_img")
+  previewImg.classList.remove("hover:scale-125");
+  clonedNode.style.setProperty('--width', nodeWidth);
+  clonedNode.style.setProperty('--height', nodeHeight);
+  clonedNode.style.setProperty('--top', nodeTop);
+  clonedNode.style.setProperty('--left', nodeLeft);
+})
+}
+
+function delay (URL) {
+  setTimeout( function() { window.location = URL }, 70000 );
+}
