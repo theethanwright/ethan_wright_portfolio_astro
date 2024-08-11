9f0eb5bd690c916f70e6c421f323215fae3e8f1c
theethanwright <theethanwright@gmail.com>
2024-08-10 21:36:18 -0400

    Fixing stuff

--- public/app.js
+++ public/app.js
@@ -9,18 +9,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.reveal');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// function openNav() {
-//   document.getElementById("menu").style.transform = "translateX(0%)";
-//   document.getElementById("menu").style.zIndex = "2";
-//   document.getElementById("navbar").style.zIndex = "1";
-// }
-
-// function closeNav() {
-//   document.getElementById("menu").style.transform = "translateX(100%)";
-//   document.getElementById("menu").style.zIndex = "1";
-//   document.getElementById("navbar").style.zIndex = "2";
-// }
-
 function toggleMenu() {
   const sidebar = document.getElementById('menu');
   sidebar.classList.toggle('open');
